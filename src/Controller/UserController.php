8b19dae668f1f5f2f429244de45ad8f460bb6836
Yannick6633 <yanlechaka@sfr.fr>
2020-09-15 18:51:44 +0200

    Création form

---
+++ src/Controller/UserController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class UserController extends AbstractController
+{
+
+    /**
+     * @var UserRepository
+     */
+    private $repository;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * UsersController constructor
+     * @param UserRepository $repository
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(UserRepository $repository, EntityManagerInterface $entityManager)
+    {
+       $this->repository = $repository;
+       $this->entityManager = $entityManager; 
+    }
+
+    /**
+     * @Route("/user", name="user_user")
+     */
+    public function index(UserRepository $repository)
+    {
+        $user = $repository->findAll();
+
+        return $this->render('user/indexUser.html.twig', [
+            'user' => $user
+        ]);
+    }
+
+     /**
+     * @Route("/user/creation", name="user_creation")
+     */
+    public function add(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('user_user');
+        }
+        
+
+
+        return $this->render('user/creation.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+            
+        ]);
+    }
+}
